1d583b5bff20f1665377ef66fbf9d0866f48efb0
송가영 <song-gayeong@song-gayeong-ui-MacBookAir.local>
2022-11-14 10:56:33 +0900

    added form and modified mealitem's id

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
@@ -8,10 +9,12 @@ const MealItem = (props) => {
     <li className={classes.meal}>
       <div>
         <h3 className={classes.name}>{props.name}</h3>
-        <div className={classes.decription}>{props.description}</div>
+        <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm id={props.id} />
+      </div>
     </li>
   );
 };
